cdf49dcc059f65f269f16f7909bdc3c869c85b15
micki-eustache <m.eustache001@albright.edu>
2024-10-17 14:07:22 -0400

    edit 07_10

--- 07_user_input_and_while_loops/10_dream_vacation.py
+++ 07_user_input_and_while_loops/10_dream_vacation.py
@@ -2,11 +2,22 @@ vacations = {}
 polling_active = True
 
 while polling_active:
-    user = input('Enter your name:  ')
-    vacay = input("What's your dream vacation?  ")
+    user = input('\tEnter your name:  ')
+    vacay = input("\tWhat's your dream vacation?  ")
     vacations[user] = vacay
 
-    cont = input('Anyone else want to participate in our poll? (y/n)')
-    if cont == 'n':
-        polling_active = False
-    
+    cont = ''
+    while cont != 'y':
+        cont = input('\nAnyone else want to participate in our poll? (y/n)')
+        cont = cont.lower()
+        if cont == 'n':
+            polling_active = False
+            break
+        elif cont == 'y':
+            polling_active = True
+        else:
+            print('\tError. Please enter valid input.')
+
+print('\n')
+for user, vacay in vacations.items():
+    print(f'{user.title()} would like to go to {vacay.title()}.')
